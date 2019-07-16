6fd372feff5057aa61003d131090b0992048afdf
bquizza <blaine555@gmail.com>
2019-07-16 16:59:02 -0600

    finished

--- src/App.js
+++ src/App.js
@@ -1,6 +1,12 @@
 import React from "react";
 import "./App.css";
 // STEP 4 - import the button and display components
+
+import { Numbers } from "./components/ButtonComponents/NumberButtons/Numbers";
+import { Operators } from "./components/ButtonComponents/OperatorButtons/Operators";
+import { Specials } from "./components/ButtonComponents/SpecialButtons/Specials";
+import { Display } from "./components/DisplayComponents/Display";
+
 // Don't forget to import any extra css/scss files you build into the correct component
 
 // Logo has already been provided for you. Do the same for the remaining components
@@ -18,6 +24,15 @@ function App() {
       <Logo />
       <div className="App">
         {/* STEP 4 - Render your components here and be sure to properly import/export all files */}
+        <Display />
+        <div className="buttonsContainer">
+          <div className='leftButtons'>
+            <Specials />
+            <Numbers />
+          </div>
+          <Operators />
+        </div>
+
       </div>
     </div>
   );
--- src/components/ButtonComponents/NumberButtons/NumberButton.js
+++ src/components/ButtonComponents/NumberButtons/NumberButton.js
@@ -1,9 +1,11 @@
 import React from "react";
 
-const NumberButton = () => {
+
+export const NumberButton = (props) => {
   return (
-    <>
+    <button>
       {/* Display a button element rendering the data being passed down from the parent container on props */}
-    </>
+      {props.num}
+    </button>
   );
 };
